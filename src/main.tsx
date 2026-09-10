b330ad07baad8e07237bf956247d0b7d0d0c3e7b
YUNTIANMI <YUNTIANMI@users.noreply.github.com>
2026-09-10 18:10:12 +0800

    feat(router): 装配 /login 与 /admin 路由入口

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,22 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
 import './styles.css'
 import App from './App'
+import { AuthProvider } from './auth/AuthProvider'
 import { StoreProvider } from './store'
 import { ToastProvider } from './toast'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <StoreProvider>
-      <ToastProvider>
-        <App />
-      </ToastProvider>
-    </StoreProvider>
+    <BrowserRouter>
+      <AuthProvider>
+        <StoreProvider>
+          <ToastProvider>
+            <App />
+          </ToastProvider>
+        </StoreProvider>
+      </AuthProvider>
+    </BrowserRouter>
   </StrictMode>,
 )
